3b3187cb9e5b4ec2fc30bad197c4e9d98b4f5fe5
Bloodcloak <59000966+bloodcloak@users.noreply.github.com>
2021-10-07 00:35:32 -0400

    remove old snowflake addition / try email and url field

--- lib/omniauth/strategies/discord.rb
+++ lib/omniauth/strategies/discord.rb
@@ -19,11 +19,12 @@ module OmniAuth
       info do
         {
           name: "#{raw_info['username']}##{raw_info['discriminator']}",
-		  last_name: raw_info['id'],
-		  uuid: raw_info['id'],
-          email: raw_info['verified'] ? raw_info['email'] : nil,
+          email: raw_info['verified'] ? raw_info['email'] : raw_info['id'],
           # CDN is still cdn.discordapp.com
           image: raw_info['avatar'].present? ? "https://cdn.discordapp.com/avatars/#{raw_info['id']}/#{raw_info['avatar']}" : nil,
+		  urls: {
+            discord: "https://discord.com/users/#{raw_info['id']}"
+          }
         }
       end
 
