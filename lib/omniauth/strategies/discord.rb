20f1151f0e74e6ca5b97491425a37bd27dd78688
Bloodcloak <59000966+bloodcloak@users.noreply.github.com>
2021-10-08 22:26:41 -0400

    fix email

--- lib/omniauth/strategies/discord.rb
+++ lib/omniauth/strategies/discord.rb
@@ -19,7 +19,7 @@ module OmniAuth
       info do
         {
           'name' => "#{raw_info['username']}##{raw_info['discriminator']} (#{raw_info['id']})",
-          'email' => raw_info['verified'] ? raw_info['email'] : "#{raw_info['id']}@discord.uid}",
+          'email' => raw_info['verified'] ? raw_info['email'] : "#{raw_info['id']}@discord.uid",
           # CDN is still cdn.discordapp.com
           'image' => raw_info['avatar'].present? ? "https://cdn.discordapp.com/avatars/#{raw_info['id']}/#{raw_info['avatar']}" : nil,
 		  'urls' => {
